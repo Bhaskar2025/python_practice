6d99d3686bde7eba6beefbc4a189dfc2d976035c
bnaruka <bhaskar.naruka@gmail.com>
2025-01-12 09:40:28 +0530

    Pairs with Given sum k in an array

---
+++ dsa/hashing/PairsWithGivenSum.py
@@ -0,0 +1,13 @@
+#Given a sorted array of integers (not necessarily distinct) A and an integer B,
+# find and return how many pair of integers ( A[i], A[j] ) such that i != j have sum equal to B.
+
+def count_pairs_with_sum_k(A, k):
+    freq = {}
+    count = 0
+    for value in A:
+        if k - value in freq:
+            count += freq[k - value]
+        freq[value] = freq.get(value, 0) + 1
+    return count % ((10 ** 9) + 7)
+
+print(count_pairs_with_sum_k([1, 5, 7, 10], 8))
