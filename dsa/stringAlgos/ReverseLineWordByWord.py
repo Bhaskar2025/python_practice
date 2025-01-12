234753653676dd6739da2c6136ff71a1e8941daf
bnaruka <bhaskar.naruka@gmail.com>
2025-01-12 14:58:42 +0530

    Reverse line word by word

---
+++ dsa/stringAlgos/ReverseLineWordByWord.py
@@ -0,0 +1,7 @@
+def reverse_string(input_str):
+    # arr = input_str.split()
+    # result = " ".join(reversed(arr)) # arr[::-1] also reverses the string
+    # return result
+    return  input_str[0]
+
+print(reverse_string("Bhaskar is learning Python"))
